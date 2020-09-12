430927e7439f0ba084cbca58db71df1ef66dc814
Dell <jhanu773@gmail.com>
2020-09-12 19:26:07 +0530

    toast message

---
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.Toast);
+
+        b.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT ).show();
+
+    }
+}
